bd2022f8e908a1ae2d0d856cf38309eb0bc9a880
mandrejevic <milan.andrejevic@gmail.com>
2023-07-13 08:28:20 +0200

    hide api key

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -2,19 +2,11 @@
 /* eslint-disable no-unused-vars */
 
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.17.1/firebase-app-compat.js"
+  "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"
 );
 importScripts(
-  "https://www.gstatic.com/firebasejs/9.17.1/firebase-messaging-compat.js"
+  "https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js"
 );
 
-firebase.initializeApp({
-  apiKey: "AIzaSyBj5uKo0P_mir_ChQ_syx_kUQ_g7nkNy6M",
-  authDomain: "andsnews.firebaseapp.com",
-  databaseURL: "https://andsnews.firebaseio.com",
-  projectId: "andsnews",
-  storageBucket: "andsnews.appspot.com",
-  messagingSenderId: "719127177629",
-  appId: "1:719127177629:web:5e1335058fa70551186774",
-});
+firebase.initializeApp({});
 const messaging = firebase.messaging();
