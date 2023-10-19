aa0e00316518883f8d0ae19891c73a9b181d63bf
mandrejevic <milan.andrejevic@gmail.com>
2023-10-19 19:00:05 +0200

    new firebase-messaging-sw.js

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,47 +1,17 @@
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-app-compat.js"
-);
-importScripts(
-  "https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js"
-);
+const { initializeApp } = require("firebase/app");
+const { getMessaging, onBackgroundMessage } = require("firebase/messaging/sw");
+const config = require("../config.json");
 
-fetch("../config.json")
-  .then((res) => res.json())
-  .then((data) => {
-    firebase.initializeApp(data.firebase);
-    const messaging = firebase.messaging();
+const firebaseApp = initializeApp(config.firebase);
+const messaging = getMessaging(firebaseApp);
 
-    messaging.onBackgroundMessage(
-      ({ notification: { title, body, image } }) => {
-        self.registration.showNotification(title, {
-          body,
-          icon: image || "/icons/favicon-32x32.png",
-        });
-      }
-    );
-  });
+onBackgroundMessage(messaging, (payload) => {
+  // Customize notification here
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: image || "icons/favicon-32x32.png",
+  };
 
-// import { initializeApp } from "firebase/app";
-// import { getMessaging } from "firebase/messaging/sw";
-// import { onBackgroundMessage } from "firebase/messaging/sw";
-
-// fetch("../config.json")
-//   .then((res) => res.json())
-//   .then((data) => {
-//     const firebaseApp = initializeApp(data.firebase);
-//     const messaging = getMessaging(firebaseApp);
-
-//     onBackgroundMessage(messaging, (payload) => {
-//       // Customize notification here
-//       const notificationTitle = payload.notification.title;
-//       const notificationOptions = {
-//         body: payload.notification.body,
-//         icon: "icons/favicon-32x32.png",
-//       };
-
-//       self.registration.showNotification(
-//         notificationTitle,
-//         notificationOptions
-//       );
-//     });
-//   });
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
