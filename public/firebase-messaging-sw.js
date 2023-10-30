ea45841d0adb7fe2285dc06a7316d882f9d1c93a
mandrejevic <milan.andrejevic@gmail.com>
2023-10-30 12:43:54 +0100

    .

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -5,36 +5,31 @@ importScripts(
   "https://www.gstatic.com/firebasejs/9.22.2/firebase-messaging-compat.js"
 );
 
-try {
-  firebase.initializeApp({
-    apiKey: "AIzaSyBHV3J3GAEPiTU2MCrhuRI4F9mWzdjw6B0",
-    authDomain: "andrejevici.firebaseapp.com",
-    databaseURL: "https://andrejevici.firebaseio.com",
-    projectId: "andrejevici",
-    storageBucket: "andrejevici.appspot.com",
-    messagingSenderId: "183441678976",
-    appId: "1:183441678976:web:3f87f36ff673545d3fbc65",
-    measurementId: "G-4HF1XHQ8Y6",
-  });
-  const messaging = firebase.messaging();
+firebase.initializeApp({
+  apiKey: "AIzaSyBHV3J3GAEPiTU2MCrhuRI4F9mWzdjw6B0",
+  authDomain: "andrejevici.firebaseapp.com",
+  databaseURL: "https://andrejevici.firebaseio.com",
+  projectId: "andrejevici",
+  storageBucket: "andrejevici.appspot.com",
+  messagingSenderId: "183441678976",
+  appId: "1:183441678976:web:3f87f36ff673545d3fbc65",
+  measurementId: "G-4HF1XHQ8Y6",
+});
+const messaging = firebase.messaging();
 
-  // messaging.onBackgroundMessage((payload) => {
-  //   console.log(payload);
-  //   // Customize notification here
-  //   const title = payload.notification.title;
-  //   const options = {
-  //     body: payload.notification.body,
-  //     image: "icons/favicon-32x32.png",
-  //   };
+messaging.onBackgroundMessage((payload) => {
+  console.log(payload);
+  // Customize notification here
+  const title = payload.notification.title;
+  const options = {
+    body: payload.notification.body,
+    image: "icons/favicon-32x32.png",
+  };
 
-  //   self.registration.showNotification(title, options);
-  // });
-
+  self.registration.showNotification(title, options);
   // self.addEventListener("notificationclick", function (event) {
   //   clients.openWindow(payload.data.link);
   // });
-} catch (error) {
-  console.log(error);
 }
 
 // import { initializeApp } from "firebase/app";
