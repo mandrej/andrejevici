9198114f007ff494ac8b642d4247f04a44515d4a
mandrej <milan.andrejevic@gmail.com>
2024-02-06 12:43:35 +0100

    getImageDimensions

--- src/helpers/index.js
+++ src/helpers/index.js
@@ -101,6 +101,18 @@ export const thumbUrl = (filename) => {
     thumbName(filename),
   ].join("/");
 };
+export const getImageDimensions = (url) => {
+  return new Promise((resolve, reject) => {
+    const img = new Image();
+    img.onload = () =>
+      resolve({
+        width: img.width,
+        height: img.height,
+      });
+    img.onerror = (error) => reject(error);
+    img.src = url;
+  });
+};
 export {
   CONFIG,
   months,
