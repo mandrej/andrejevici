55f194ce96ca73896d691cb44b10fc225ee51178
mandrej <milan.andrejevic@gmail.com>
2024-07-18 10:33:19 +0200

    NEW EXIF READER

--- src/helpers/exif.js
+++ src/helpers/exif.js
@@ -1,92 +1,89 @@
-import exifr from "exifr";
+import exifReader from "exifreader";
 import { has } from "lodash";
 import { formatDatum } from "./index";
 
-const values = [
-  "Make",
-  "Model",
-  "LensModel",
-  "DateTimeOriginal",
-  "FNumber",
-  "ExposureTime",
-  "FocalLength",
-  "ISO",
-  "Flash",
-  "GPSLatitude",
-  "GPSLatitudeRef",
-  "GPSLongitude",
-  "GPSLongitudeRef",
-];
-const LENSES = {
-  "Nikon NIKKOR Z 24-70mm f4 S": "NIKKOR Z 24-70mm f4 S",
-  "70-300 mm f4.5-5.6": "70.0-300.0 mm f4.5-5.6",
-  "Canon EF-S 17-55mm f2.8 IS USM": "EF-S17-55mm f2.8 IS USM",
-  "Canon EF 100mm f2.8 Macro USM": "EF100mm f2.8 Macro USM",
-  "Canon EF 50mm f1.8 STM": "EF50mm f1.8 STM",
-};
-const decimal_coords = (coords, gpsRef) => {
-  let decimal_degrees = coords[0] + coords[1] / 60 + coords[2] / 3600;
-  if (gpsRef == "S" || gpsRef == "W") decimal_degrees = -decimal_degrees;
-  return decimal_degrees;
-};
+// const LENSES = {
+//   "Nikon NIKKOR Z 24-70mm f4 S": "NIKKOR Z 24-70mm f4 S",
+//   "70-300 mm f4.5-5.6": "70.0-300.0 mm f4.5-5.6",
+//   "Canon EF-S 17-55mm f2.8 IS USM": "EF-S17-55mm f2.8 IS USM",
+//   "Canon EF 100mm f2.8 Macro USM": "EF100mm f2.8 Macro USM",
+//   "Canon EF 50mm f1.8 STM": "EF50mm f1.8 STM",
+// };
 
 const readExif = async (url) => {
-  const exif = { model: "UNKNOWN", date: new Date() };
-  const data = await exifr.parse(url, values);
-  // console.log(data);
+  const result = { model: "UNKNOWN", date: formatDatum(new Date()) };
+  const tags = await exifReader.load(url, { expanded: true });
+  if (tags.exif) delete tags.exif["MakerNote"];
+  if (tags.Thumbnail) delete tags.Thumbnail;
+  if (tags.icc) delete tags.icc;
+  if (tags.iptc) delete tags.iptc;
+  if (tags.xmp) delete tags.xmp;
 
-  if (has(data, "Make") && has(data, "Model")) {
-    const make = data.Make.replace("/", "");
-    const model = data.Model.replace("/", "");
+  // EXIF
+  const exif = tags.exif;
+  // console.log(exif);
+  if (has(exif, "Make") && has(exif, "Model")) {
+    const make = exif.Make.description.replace("/", "");
+    const model = exif.Model.description.replace("/", "");
     const makelArr = make.split(" ");
     const modelArr = model.split(" ");
     for (let it of makelArr) {
       if (modelArr.includes(it)) {
-        exif.model = model;
+        result.model = model;
         break;
       } else {
-        exif.model = make + " " + model;
+        result.model = make + " " + model;
       }
     }
   }
-  if (has(data, "LensModel")) {
-    exif.lens = data.LensModel.replace("/", "");
+  if (has(exif, "LensModel")) {
+    result.lens = exif.LensModel.description.replace("/", "");
   }
-  if (has(data, "DateTimeOriginal")) {
-    const datum = new Date(Date.parse(data.DateTimeOriginal));
-    exif.date = formatDatum(data.DateTimeOriginal);
-    exif.year = datum.getFullYear();
-    exif.month = datum.getMonth() + 1;
-    exif.day = datum.getDate();
+  if (has(exif, "DateTimeOriginal")) {
+    const datum = new Date(
+      Date(exif.DateTimeOriginal.description, "yyyy:MM:dd HH:mm:ss")
+    );
+    result.date = formatDatum(datum);
+    result.year = datum.getFullYear();
+    result.month = datum.getMonth() + 1;
+    result.day = datum.getDate();
   }
-  if (has(data, "FNumber")) exif.aperture = data.FNumber;
-  if (has(data, "ExposureTime")) {
-    if (data.ExposureTime <= 0.1) {
-      exif.shutter = "1/" + parseInt(1 / data.ExposureTime);
+  if (has(exif, "ApertureValue"))
+    result.aperture = parseFloat(exif.ApertureValue.description);
+  if (has(exif, "ExposureTime")) {
+    const shutter = exif.ExposureTime.value[0] / exif.ExposureTime.value[1];
+    if (shutter <= 0.1) {
+      result.shutter = "1/" + parseInt(1 / shutter);
     } else {
-      exif.shutter = "" + data.ExposureTime;
+      result.shutter = "" + shutter;
     }
   }
-  if (has(data, "FocalLength")) exif.focal_length = parseInt(data.FocalLength);
-  if (has(data, "ISO")) exif.iso = data.ISO;
-  if (has(data, "Flash"))
-    exif.flash = data.Flash.startsWith("Flash did not") ? false : true;
-  if (
-    has(data, "GPSLatitude") &&
-    has(data, "GPSLatitudeRef") &&
-    has(data, "GPSLongitude") &&
-    has(data, "GPSLongitudeRef")
-  ) {
-    if (!isNaN(data.GPSLatitude) && !isNaN(data.GPSLongitude)) {
-      exif.loc = [
-        decimal_coords(data.GPSLatitude, data.GPSLatitudeRef),
-        decimal_coords(data.GPSLongitude, data.GPSLongitudeRef),
-      ];
-    }
+  if (has(exif, "FocalLength"))
+    result.focal_length = parseInt(exif.FocalLength.description);
+  if (has(exif, "ISOSpeedRatings"))
+    result.iso = exif.ISOSpeedRatings.description;
+  if (has(exif, "Flash"))
+    result.flash = exif.Flash.description.startsWith("Flash did not")
+      ? false
+      : true;
+
+  // DIMENSIONS
+  if (has(tags.file, "Image Height") && has(tags.file, "Image Width")) {
+    result.dim = [
+      tags.file["Image Width"].value,
+      tags.file["Image Height"].value,
+    ];
   }
 
-  // console.log(exif);
-  return exif;
+  // GPS
+  console.log(tags.gps);
+  if (has(tags.gps, "Latitude") && has(tags.gps, "Longitude")) {
+    result.loc =
+      "" + tags.gps.Latitude.toFixed(6) + ", " + tags.gps.Longitude.toFixed(6);
+  }
+
+  console.log(result);
+  return result;
 };
 
 export default readExif;
