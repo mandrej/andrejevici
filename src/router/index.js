f743e0bf712a1282407ee1f03dfe027b6c86d8ce
mandrej <milan.andrejevic@gmail.com>
2024-08-18 13:28:32 +0200

    NO boot guard

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,5 @@
 import { nextTick } from "vue";
+import { useUserStore } from "../stores/user";
 import { createRouter, createWebHistory } from "vue-router";
 import routes from "./routes";
 
@@ -6,6 +7,17 @@ const router = createRouter({
   scrollBehavior: () => ({ left: 0, top: 0 }),
   routes,
   history: createWebHistory(process.env.VUE_ROUTER_BASE),
+});
+
+router.beforeEach((to, from) => {
+  const auth = useUserStore();
+  const user = auth.user;
+
+  if (to.meta.requiresAuth && !user.isAuthorized) {
+    return { name: "401", replace: true };
+  } else if (to.meta.requiresAdmin && !user.isAdmin) {
+    return { name: "401", replace: true };
+  }
 });
 
 router.afterEach((to, from) => {
