f76914d6341fe363e16025bbe405af2bb64d2507
mandrej <milan.andrejevic@gmail.com>
2025-12-01 17:08:08 +0100

    auto dark

--- quasar.config.ts
+++ quasar.config.ts
@@ -91,7 +91,7 @@ export default defineConfig((/* ctx */) => {
     // https://v2.quasar.dev/quasar-cli-vite/quasar-config-file#framework
     framework: {
       config: {
-        dark: false,
+        dark: 'auto',
       },
 
       // iconSet: 'material-icons', // Quasar icon set
